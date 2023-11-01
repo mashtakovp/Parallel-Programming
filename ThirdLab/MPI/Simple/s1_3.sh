7215923ac2d271252b0a29caec9dac89d9a72ad6
Pavel Mashtakov <mashtakovqa@gmail.com>
2023-11-01 21:38:29 +0400

    commands

---
+++ ThirdLab/MPI/Simple/s1_3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=Mashtakoff
+#SBATCH --time=00:03:00
+#SBATCH --nodes=1 --ntasks-per-node=3
+#SBATCH --mem=1gb
+
+export I_MPI_LIBRARY=/usr/lib64/slurm/mpi_pmi2.so
+srun --mpi=pmi2 ./s1
---
+++ ThirdLab/OpenMP/s2612.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --job-name=Mashtakoff
+#SBATCH --time=00:03:00
+#SBATCH --nodes=1 --ntasks-per-node=1
+#SBATCH --mem=1gb
+./s2612
